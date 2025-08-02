0abc236b2833fc7cd7e24dc8972f6fadab5e1484
JIHOON LEE <eziya76@gmail.com>
2025-08-02 15:03:56 +0900

    added stm32h747i example
    added stm32h747i example

---
+++ STM32H747I_RTOS_LWIP/CM4/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32h747xihx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32h747xihx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32h747xihx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I../Core/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc -I../../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../../Drivers/CMSIS/Include -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Core-2f-Startup
+
+clean-Core-2f-Startup:
+	-$(RM) ./Core/Startup/startup_stm32h747xihx.d ./Core/Startup/startup_stm32h747xihx.o
+
+.PHONY: clean-Core-2f-Startup
+
